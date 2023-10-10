5980af35e0ebc5c11b82f694a81cb7c0bc3641fe
Andre Regino <reginopanai@gmail.com>
2023-10-09 23:15:23 -0600

    Chamber website first draft

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,13 @@
+let today = new Date();
+document.querySelector("#currentYear").innerHTML = `&copy ${today.getFullYear()}`;
+
+document.querySelector("#lastModified").textContent = document.lastModified;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});

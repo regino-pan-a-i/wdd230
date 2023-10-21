dbf2a1494f71eb1334ccdfcc13a623a1230de954
Andre Regino <reginopanai@gmail.com>
2023-10-21 09:01:32 -0600

    hero image added to home page

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,23 @@
+function showWindChill(temp, speed){
+    const windChillSpan = document.getElementById("windchill");
+
+    let message = "N/A";
+
+    if (temp <= 50 && speed > 3){
+
+        let chillFactor = Math.pow(speed, 0.16);
+        let chill = Math.round(35.74 + (0.6215 * temp) - (35.75 * chillFactor) + (0.4275 * temp * chillFactor));
+        message = `${chill}`;
+    }
+    
+
+
+    windChillSpan.textContent = "Wind Chill " + message;
+}
+
+const temperatureSpan = document.getElementById("temperature");
+const windspeedSpan = document.getElementById("windspeed");
+const tepmerature = parseInt(temperatureSpan.textContent);
+const windspeed = parseInt(windspeedSpan.textContent);
+
+showWindChill(tepmerature, windspeed)

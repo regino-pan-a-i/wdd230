0e820216e3b89b304b8510c9d2dd046fd8ea5c68
Andre Regino <reginopanai@gmail.com>
2023-11-06 22:40:53 -0700

    Join page

--- scripts/form.js
+++ scripts/form.js
@@ -5,8 +5,30 @@ rating.addEventListener('change', ()=>{
 
 })
 
-const confirm = document.getElementById("confirm")
+const confirm = document.getElementById("confirm");
+const password = document.getElementById("password");
+const message = document.querySelector("#message");
+const regex = /[\w]{8,}/g;
+
 confirm.addEventListener('blur', ()=>{
-    const password = document.getElementById("password")
+    if (password.value.match(regex)){
+        if (confirm.value != password.value){      
+            confirm.style.boxShadow = "3px 3px 3px red";
+            password.style.boxShadow = "3px 3px 3px red"
+            message.textContent = "❗PASSWORDS DO NOT MATCH!";
+            message.style.visibility = "show";
+            confirm.value = '';
+            password.value = '';
+            password.focus();
+        }else{
+            message.style.display = "none";
+            confirm.style.boxShadow = "3px 3px 3px green";
+            password.style.boxShadow = "3px 3px 3px green"
+            
+        }
+    }else{
+        confirm.style.boxShadow = "3px 3px 3px red";
+    }
 
 })
+

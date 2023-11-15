068d62d758bbc9b9eb515bd4c89e387229bb5c00
Andre Regino <reginopanai@gmail.com>
2023-11-14 21:55:13 -0700

    discovery final touches

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -28,19 +28,31 @@ async function getMemberData(){
 
 getMemberData();
 
-const gridButton = document.getElementById('gridbutton');
-const listButton = document.getElementById('listbutton');
+const gridButton = document.getElementById('gridButton');
+const listButton = document.getElementById('listButton');
 const container = document.querySelector('#members')
 
 gridButton.addEventListener('click', ()=>{
-    gridButton.classList.add('active')
-    container.classList.remove('list');
-    container.classList.add('grid');
+    if(!gridButton.classList.contains('active')){
+        gridButton.classList.add('active');
+        gridButton.classList.remove('inactive');
+        listButton.classList.remove('active');
+        listButton.classList.add('inactive');
+        gridButton.classList.add('active');
+        container.classList.remove('list');
+        container.classList.add('grid');
+    }
     
 });
 
 listButton.addEventListener('click', () => {
-    listButton.classList.add('active')
-    container.classList.remove('grid');
-    container.classList.add('list');
+    if(!listButton.classList.contains('active')){
+        listButton.classList.add('active');
+        listButton.classList.remove('inactive');
+        gridButton.classList.remove('active')
+        gridButton.classList.add('inactive');
+        listButton.classList.add('active')
+        container.classList.remove('grid');
+        container.classList.add('list');
+    }
 });

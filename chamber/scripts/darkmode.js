11d3ae47e9233780337eb92509d1539c07dd2761
Andre Regino <reginopanai@gmail.com>
2023-11-18 12:34:42 -0700

    fixed dark mode

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -3,12 +3,11 @@ const bodyel = document.querySelector('body');
 
 darkmode.addEventListener('click', ()=>{
     if(darkmode.textContent == 'DARK'){
-        document.documentElement.style.setProperty('--primary-color', '#31081F');
+        document.documentElement.style.setProperty('--primary-color', '#962705');
         document.documentElement.style.setProperty('--secondary-color', '#333333');
         document.documentElement.style.setProperty('--accent1-color', '#0F1E22');
         document.documentElement.style.setProperty('--accent2-color', '#38060B');
-        document.documentElement.style.setProperty('--nav-hover-link-color', '#31081F');
-        document.documentElement.style.setProperty('--nav-hover-background-color', 'white');
+        document.documentElement.style.setProperty('--nav-hover-background-color', '#962705');
         
         bodyel.style.backgroundColor = '#04395E';
         bodyel.style.color = 'white'
@@ -17,10 +16,12 @@ darkmode.addEventListener('click', ()=>{
         document.documentElement.style.setProperty('--primary-color', '#601700');
         document.documentElement.style.setProperty('--secondary-color', '#595959');
         document.documentElement.style.setProperty('--accent1-color', '#DCE0D9');
-        document.documentElement.style.setProperty('--accent2-color', '#6B0F1A');
+        document.documentElement.style.setProperty('--accent2-color', '#601600b2');
+        document.documentElement.style.setProperty('--nav-hover-link-color', 'white');
+        document.documentElement.style.setProperty('--nav-hover-background-color', '#601700');
         bodyel.style.backgroundColor = 'white';
         bodyel.style.color = 'black'
-
+        
         darkmode.textContent = 'DARK'
     }
 })

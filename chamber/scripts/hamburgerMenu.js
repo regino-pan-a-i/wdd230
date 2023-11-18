380d37db588a7863022770fe96dfe5f582200604
Andre Regino <reginopanai@gmail.com>
2023-11-18 12:21:54 -0700

    fixed hamburger menu

--- chamber/scripts/hamburgerMenu.js
+++ chamber/scripts/hamburgerMenu.js
@@ -1,9 +1,9 @@
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
-// const nav = documenet.querySeletor(nav);
+const nav = document.querySelector('nav');
 // Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
 hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
-	// nav.classList.toggle('show');
+	nav.classList.toggle('show');
 });
